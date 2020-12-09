bb8e4b84072969e00c5effb3633ce98ac19b0991
Ethan Sifferman <ethan@sifferman.dev>
2020-12-09 05:39:29 -0800

    simple firebase interface added

--- public/app.js
+++ public/app.js
@@ -1,17 +1,47 @@
 
 /* app.js */
 
-document.addEventListener( "DOMContentLoaded", event => {
 
-    const app = firebase.app();
-    const db = firebase.firestore();
+const app   = firebase.app();
+const db    = firebase.firestore();
 
-    const firstPost = db.collection('posts').doc('firstpost');
+function handle_submitForm() {
+    let name    = document.getElementById("name");
+    let number  = returnCheckedRadioFromName("number");
+    let submit  = document.getElementById("Submit");
 
-    firstPost.get()
-        .then( doc => {
-            const data = doc.data();
-            document.write( data.title );
-        })
+    if ( name.value == "" || number == null )
+        return;
 
-});
+        
+    console.log( name.value );
+    console.log( number.value );
+
+    db.collection( "form_submissions" ).doc( name.value ).set({
+        name: name.value,
+        number: number.value
+    })
+    .then(function() {
+        console.log("Submission successfully written!");
+    })
+    .catch(function(error) {
+        console.error("Error writing submission: ", error);
+    });
+
+    name.value = "";
+    number.checked = false;
+    submit.disabled = true;
+
+}
+
+function returnCheckedRadioFromName( name ) {
+
+    let radios = document.getElementsByName( name );
+
+    for ( let i = 0; i < radios.length; i++ ) {
+        if ( radios[i].checked ) {
+            return radios[i]
+        }
+    }
+
+}
