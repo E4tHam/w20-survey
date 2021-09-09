14db69782c49109863b2486810126b7a8321e0a2
Ethan Sifferman <ethan@sifferman.dev>
2021-09-09 11:29:28 -0700

    added view of cost of slider

--- public/_scripts/slider.js
+++ public/_scripts/slider.js
@@ -3,7 +3,18 @@
 
 
 hasSlider                   = true;
+const SliderMin             = 1.2;
+const SliderMax             = 2.4;
+const SLiderStart           = SliderMin;
 const Slider                = document.getElementById("Slider");
+const SliderCost            = document.getElementById("SliderCost");
+
+Slider.min                  = SliderMin;
+Slider.max                  = SliderMax;
+Slider.value                = SLiderStart;
+
+SliderCost.innerHTML        = ( b * SLiderStart - a ).toFixed(2);
+
 
 Actions[ "SliderRecord" ]   = new Object();
 
@@ -12,6 +23,8 @@ let SLIDER_recent           = NaN;
 function handle_Slider() {
 
     let SLIDER_current = parseFloat( Slider.value );
+    SliderCost.innerHTML = ( b * SLIDER_current - a ).toFixed(2);
+
 
     if ( SLIDER_current == SLIDER_recent )
         return;
