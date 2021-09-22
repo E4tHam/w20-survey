6b18b91de7dd9680a4e9b0a292f6ebae6d2a37fa
E4tHam <ethan@sifferman.dev>
2021-09-21 23:43:14 -0700

    data can now be downloaded

---
+++ public/_scripts/download.js
@@ -0,0 +1,126 @@
+
+const DownloadButton = document.getElementById("DownloadButton");
+const NumComputers = document.getElementById("NumComputers");
+
+const app               = firebase.app();
+const db                = firebase.firestore();
+
+const VERSIONS = [
+    "Correlated_slider",
+    "Correlated_slider+stop",
+    "Correlated_stop",
+    "Independent_slider",
+    "Independent_slider+stop",
+    "Independent_stop"
+];
+
+var input_status = new Map();
+
+NumComputers.disabled = false;
+
+handle_NumComputers();
+function handle_NumComputers() {
+    input_status.set("SubmitButton", ( NumComputers.value !== "" ) );
+    update_DownloadButton_status();
+}
+function update_DownloadButton_status() {
+    let to_disable = false;
+    for ( const value of input_status.values() ) {
+        if ( !value )
+        to_disable = true;
+    }
+    DownloadButton.disabled = to_disable;
+}
+
+async function handle_DownloadButton() {
+    console.log("DOWNLOAD: START");
+    DownloadButton.disabled = true;
+
+    if ( !number_valid(NumComputers.value) ) {
+        alert("Invalid number given.");
+        update_DownloadButton_status();
+        return;
+    }
+    let num_computers = parseInt(NumComputers.value);
+
+    let json = new Object();
+
+    for ( const version in VERSIONS ) {
+
+        let version_json = [];
+
+        for ( let computer = 1; computer <= num_computers; computer++ ) {
+            console.log( "trying " + VERSIONS[version] + " " + computer );
+            let finished = false;
+            await db
+                .collection("submissions")
+                .doc( VERSIONS[version] )
+                .collection( ""+computer )
+                .doc("metadata")
+                .get().then((doc) => {
+                    if ( doc.data().finished ) {
+                        finished = true;
+                    }
+                }).catch( ()=>{} )
+                ;
+            if ( !finished ) continue;
+            console.log(`Downloading data from computer ${computer}.`);
+
+            let out = new Object();
+            out.data = [];
+
+            db
+                .collection("submissions")
+                .doc( VERSIONS[version] )
+                .collection( ""+computer )
+                .doc("metadata")
+                .get().then((doc) => {
+                    out.metadata = doc.data();
+                }).catch( ()=>{} );
+            db
+                .collection("submissions")
+                .doc( VERSIONS[version] )
+                .collection( ""+computer )
+                .doc("information")
+                .get().then((doc) => {
+                    out.information = doc.data();
+                }).catch( ()=>{} );
+
+            for ( let process = 0; process < 30; process++ ) {
+                await db
+                    .collection("submissions")
+                    .doc( VERSIONS[version] )
+                    .collection( ""+computer )
+                    .doc(""+process)
+                    .get().then((doc) => {
+                        out.data.push( doc.data() );
+                    }).catch( ()=>{} );
+            }
+
+            version_json.push(out);
+
+        }
+        json[VERSIONS[version]] = version_json;
+    }
+
+    update_DownloadButton_status();
+    console.log(json);
+
+
+    // download
+    let a = document.createElement('a');
+    a.href = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(json,null,2));
+    a.download = 'data.json';
+    a.click();
+
+
+    console.log("DOWNLOAD: DONE");
+}
+
+function number_valid(num) {
+    let temp = 0;
+    try {temp = parseInt(num);}
+    catch (error) {return false;}
+
+    return ( 1 <= temp && temp <= 100 );
+}
---
+++ public/_scripts/start/start.js
@@ -0,0 +1,161 @@
+
+/* start.js */
+
+
+const app           = firebase.app();
+const db            = firebase.firestore();
+
+const SubmitButton  = document.getElementById( "SubmitButton" );
+const NameInput     = document.getElementById( "NameInput" );
+const TokenInput    = document.getElementById( "TokenInput" );
+
+const CASE          = document.currentScript.getAttribute("case");
+var TOKEN           = NaN;
+
+const DATA_SET      = ( CASE.indexOf( "Independent" ) !== -1 ) ? "independent"
+                    : ( CASE.indexOf( "Correlated"  ) !== -1 ) ? "correlated"
+                    : "UNKNOWN";
+
+
+var processes       = [ "temp" ];
+db.collection( "onload_data" ).doc( DATA_SET )
+        .collection( "processes" ).doc( "document_ids" )
+    .get()
+        .then( doc => {
+            processes = doc.data().ids;
+        }).catch(function(error) {
+            console.error( "Error loading processes: ", error );
+            alert( "Please try again later." );
+        })
+;
+NameInput.disabled = false;
+TokenInput.disabled = false;
+
+
+
+
+/* Buttons */
+
+var input_status = new Map();
+
+handle_NameInput();
+function handle_NameInput() {
+    input_status.set("SubmitButton", ( NameInput.value !== "" ) );
+    update_SubmitButton_status();
+}
+handle_TokenInput();
+function handle_TokenInput() {
+    input_status.set("TokenInput", ( TokenInput.value !== "" ));
+    update_SubmitButton_status();
+}
+function update_SubmitButton_status() {
+    let to_disable = false;
+    for ( const value of input_status.values() ) {
+        if ( !value )
+        to_disable = true;
+    }
+    SubmitButton.disabled = to_disable;
+}
+
+async function handle_SubmitButton() {
+    NameInput.disabled = true;
+    TokenInput.disabled = true;
+    SubmitButton.disabled = true;
+
+    // calculate token
+    TOKEN = return_token();
+
+    if ( !token_valid() ) {
+        alert("Error: token not valid.");
+        NameInput.disabled = false;
+        TokenInput.disabled = false;
+        update_SubmitButton_status();
+        return;
+    }
+
+    // if token already exists in firebase
+    await db.collection( "submissions" ).doc( CASE )
+        .collection( TOKEN ).doc( "metadata" )
+        .get().then( doc => {
+            if ( doc.exists )
+                window.location.replace(
+                    "../survey/?"
+                    + "token=" + TOKEN
+                );
+        })
+        .catch( )
+    ;
+
+    // store all information
+    await store_information();
+
+    // set process to 0
+    // assign_process_order();
+    await store_process();
+
+    // redirect
+    window.location.replace(
+        "../survey/?"
+        + "token=" + TOKEN
+        + "&process=0"
+        + "&practice"
+    );
+}
+
+
+
+
+function assign_process_order() {
+    // placeholder code
+    let temp = processes;
+    processes = [];
+    while ( temp.length != 0 ) {
+        let rand = Math.floor( Math.random() * temp.length );
+        processes.push( temp[rand] );
+        temp.splice( rand, 1 );
+    }
+}
+
+
+function return_token() {
+    return TokenInput.value;
+}
+
+function token_valid() {
+    let temp = 0;
+    try {temp = parseInt(TOKEN);}
+    catch (error) {return false;}
+
+    return ( 1 <= temp && temp <= 100 );
+}
+
+function store_information() {
+    let date = new Date();
+    return db.collection( "submissions" ).doc( CASE )
+        .collection( TOKEN ).doc( "information" ).set({
+            name: NameInput.value,
+            date: date.toUTCString()
+        })
+    .then(function() {
+        console.log("Submission successfully written!");
+    })
+    .catch(function() {
+        alert( "[Error]: Please try again." );
+    });
+}
+
+function store_process() {
+    return db.collection( "submissions" ).doc( CASE )
+        .collection( TOKEN ).doc( "metadata" ).set({
+            finished_practice: false,
+            finished: false,
+            current: 0,
+            order: processes
+        })
+    .then(function() {
+        console.log("Process number successfully written!");
+    })
+    .catch(function() {
+        alert( "[Error]: Please try again." );
+    });
+}
